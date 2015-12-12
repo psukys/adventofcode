6823e38d3fe38b730f1c1cd94cb283ad06499506
Paulius Šukys <paul.sukys@gmail.com>
2015-12-12 12:30:57 +0100

    Solutions for day 6, https://youtu.be/eqJJIuwsNJY

---
+++ day6part2.py
@@ -0,0 +1,35 @@
+import re
+
+grid = [[0 for a in xrange(1000)] for b in xrange(1000)]
+
+
+def turn(p1, p2, action):
+    for i in range(int(p1[0]), int(p2[0]) + 1):
+        for j in range(int(p1[1]), int(p2[1]) + 1):
+            if action == 'on':
+                grid[i][j] += 1
+            elif action == 'off':
+                if grid[i][j] > 0:
+                    grid[i][j] -= 1
+            elif action == 'toggle':
+                grid[i][j] += 2
+            else:
+                print 'Uknown action!'
+
+with open('data/day6.txt') as f:
+    for line in f:
+        coordinates_search = re.search('(\d+,\d+) through (\d+,\d+)', line)
+        point1 = coordinates_search.group(1).split(',')
+        point2 = coordinates_search.group(2).split(',')
+        if 'turn on' in line:
+            turn(point1, point2, 'on')
+        elif 'turn off' in line:
+            turn(point1, point2, 'off')
+        elif 'toggle' in line:
+            turn(point1, point2, 'toggle')
+
+light_sum = 0
+for row in grid:
+    light_sum += sum(row)
+
+print light_sum
